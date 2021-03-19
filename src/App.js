a48f601f0b335629e4691109fef2a0f47d76017f
taz94kim <hyunjin_k@hotmail.com>
2021-03-19 15:43:55 +0900

    customer.js create

--- src/App.js
+++ src/App.js
@@ -4,18 +4,40 @@ import Customer from './componenets/Customer';
 
 
 
-const student = {
-  'name' : 'jini',
+const student = [{
+  'id': 1,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : 'jini1',
+  'age' : '1',
+  'addr' : 'seoul1',
+},
+{
+  'id': 2,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : 'jini2',
+  'age' : '2',
+  'addr' : 'seoul2',
+},
+{
+  'id': 3,
+  'image' : 'https://placeimg.com/64/64/any',
+  'name' : 'jini3',
   'age' : '3',
-  'addr' : 'seoul',
-}
+  'addr' : 'seoul3',
+}];
 
 
 
 
 function App() {
   return (
-    <Customer name={student.name} age= {student.age} />
+    <div>
+      {
+        student.map( (c,i)=>{ 
+          return (<Customer key={c.id} name={c.name} age= {c.age} image={c.image}/>);
+        })
+      }      
+    </div>
   );
 }
 
--- src/componenets/Customer.js
+++ src/componenets/Customer.js
@@ -2,18 +2,40 @@ import React from "react";
 
 class Customer extends React.Component{
 
-    
+    render(){
+            return (
+                <div>
+                   <CustomerInfo name={this.props.name} age={this.props.age} addr= {this.props.addr}/>
+                   <CustomerProfile name={this.props.name} image={this.props.image} />
+                </div>
+            );
+    }
+}
+
+
+class CustomerInfo extends React.Component{
 
     render(){
             return (
                 <div>
                     <p> {this.props.name}</p>
-                    <p> {this.props.age}</p>
-                    <p> {this.props.name}</p>
-                    <p> 4</p>
+                    <p> {this.props.age}</p>                   
+                    <p> {this.props.addr}</p>  
                 </div>
             );
     }
 }
+
+class CustomerProfile extends React.Component{
+    render(){
+        return(
+            <div>
+                <img src={this.props.image} alt="profile"></img>
+                <h2>{this.props.name}</h2>
+            </div>
+        );
+    }
+}
+
 
 export default Customer;
