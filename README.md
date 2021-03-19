6fe1f53f70ccd1976b55b79e8b1ec3b3f2c3cd2c
taz94kim <hyunjin_k@hotmail.com>
2021-03-19 12:01:24 +0900

    create react project

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
+
+## Management 
+테스트 중 
+
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -3,20 +3,12 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="
+    
+    ">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       <h2>Let's develop</h2>
       </header>
     </div>
   );
